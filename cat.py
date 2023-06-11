cc468ef0d1fffcf5246f3a52d081c96576606d8a
tej <pavanteja123ll@gmail.com>
2023-06-12 04:26:29 +0530

    Cat is created

---
+++ cat.py
@@ -0,0 +1,9 @@
+class Cat:
+    def eat(self):
+        print("I eat rat and drink milk")
+
+    def sleep(self):
+        print("I can Sleep 14 hours  Im lazy")
+
+    def bark(self):
+        print("I can't bark My sound is meow")
